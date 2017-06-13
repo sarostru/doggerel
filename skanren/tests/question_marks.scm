fdeb8f6456941259001afe689316b356662dc3c7
Scott Rostrup <scott.rostrup@gmail.com>
2017-06-12 23:38:12 -0400

    cons pairs

--- skanren/tests/question_marks.scm
+++ skanren/tests/question_marks.scm
@@ -58,3 +58,17 @@
 (assert-not-procedure (cons "lambda" (cons thwpt (cons phwpt ()))))
 (assert-not-procedure (cons "lambda" (cons "thwpt" (cons "phwpt" ()))))
 
+(define (assert-pair x)
+  (run-test (lambda () (pair? x)) x "is a pair"))
+
+(define (assert-not-pair x)
+  (run-test (lambda () (not (pair? x))) x "is not a pair"))
+
+(display "***********************")
+(display "Things that are pairs:")
+(display "***********************")
+(assert-pair (cons 1 2))
+(assert-pair (cons "one" 2))
+(assert-pair (cons 1 "two"))
+(assert-pair (cons (cons () ()) (cons () ())))
+
